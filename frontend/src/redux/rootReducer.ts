7d15acb276bd045d13ea9f5ba166adf3a6c1b327
MaorJ <zeusofek@gmail.com>
2023-06-25 00:51:08 +0300

    removed unused rootReducer

--- frontend/src/redux/rootReducer.ts
+++
@@ -1,8 +0,0 @@
-// import { combineReducers } from "redux";
-// import studentReducer from "./slices/student";
-
-// const rootReducer = combineReducers({
-//   student: studentReducer,
-// });
-
-// export default rootReducer;
